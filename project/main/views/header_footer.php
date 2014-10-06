e1307770e5332e53e0614c36f3ab524f54e04900
Michael Alaimo <malaimo923@gmail.com>
2014-10-05 20:02:56 -0400

    Make HTML5 default template.  Still need to improve head usage.

--- project/main/views/header_footer.php
+++ project/main/views/header_footer.php
@@ -1,14 +1,17 @@
 <?php
 
 $__v_header = <<<EOT
-<?xml version="1.0" encoding="utf-8" ?> 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		{$head}
+	</head>
+	<body>	
 EOT;
 
-$__v_footer = <<<EOT
 
+$__v_footer = <<<EOT
+	</body>
 </html>
 EOT;
