f894ae82a3a3f7f1520c8dcb4c707286a3c071a0
Michael Alaimo <malaimo923@gmail.com>
2014-11-16 17:44:09 -0500

    Removed common code, it is now in php_common

--- classes/collections/CurlOptionGroupStorage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace \local\classes\collections;
-
-use \local\classes\curl\Opt;
-
-class CurlOptionGroupStorage extends SplObjectStorage {
-	protected $objectType = '\local\classes\curl\Opt';
-}
