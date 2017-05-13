fcd4fa8b000c7fd88e2fbac8ec23f6bf07c815f7
Michael Alaimo <malaimo923@gmail.com>
2017-05-12 23:22:34 -0400

    Add test for current page in RequestUri object being active

--- classes/request/RequestUrl.php
+++ classes/request/RequestUrl.php
@@ -8,7 +8,13 @@ use \common\url\Main as url;
 class RequestUrl extends url {
 	
 	public function isReferrer() {
-		$url = new urll($_SERVER['REFERER']);
+		$url = new url($_SERVER['REFERER']);
+		
+		return $this->compare($url);
+	}
+	
+	public function isCurrent() {
+		$url = new url($_SERVER['REQUEST_URI']);
 		
 		return $this->compare($url);
 	}
